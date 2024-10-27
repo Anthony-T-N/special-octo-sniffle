d41f669774b6ff5cd777b3a53970e605673ad6fc
Anthony <53560209+Anthony-T-N@users.noreply.github.com>
2024-10-27 23:37:25 +1100

    Update log_generator.py

--- log_generator.py
+++ log_generator.py
@@ -4,7 +4,8 @@ def execute():
   auditd_types = ["SYSCALL", "USER_AUTH", "USER_CMD", "LOGIN", "CWD", "PATH", "FILE"]
   selected_audit_type = auditd_types[random.randint(0, len(auditd_types)) - 1]
   pid_number = random.randint(1000, 9999)
-  print(f"type={selected_audit_type} msg=audit(XXXXXXXXXXXXXXX): pid={pid_number} uid=")
+  uid_number = random.randint(0, 65535)
+  print(f"type={selected_audit_type} msg=audit(XXXXXXXXXXXXXXX): pid={pid_number} uid={uid_number}")
 
 if __name__ == "__main__":
   execute()
